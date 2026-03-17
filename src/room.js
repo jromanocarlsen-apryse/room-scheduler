ecad334c8470ea629dc9b80ccad14f4d828fb36c
jacobrc10 <jromanocarlsen@gmail.com>
2026-03-17 16:31:43 -0700

    Created room class to track list of meetings

---
+++ src/room.js
@@ -0,0 +1,30 @@
+export class Room {
+
+    //** Map of meeting id to meeting object */
+    meetingList;
+
+    constructor() {
+        this.meetingList = new Map();
+    }
+
+    addMeeting(meetingId, startTime, endTime) {
+        // TODO
+    }
+
+    removeMeeting(meetingId) {
+        // TODO
+    }
+
+    getNextAvailableTime() {
+        // TODO
+    }
+
+    getMeetings() {
+        // TODO
+    }
+
+    isEmpty() {
+        // TODO
+    }
+    
+}
