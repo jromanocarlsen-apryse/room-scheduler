bbb3cdc5eaee68d7dd94b9d0b23400c8d175140c
jacobrc10 <jromanocarlsen@gmail.com>
2026-03-17 17:36:01 -0700

    Added heap class

---
+++ src/heap.js
@@ -0,0 +1,38 @@
+export class Heap {
+    
+    //** Array to store the heap elements */
+    heap;
+
+    constructor() {
+        this.heap = [];
+    }
+
+    insert(node){
+        // TODO
+    }
+
+    peek() {
+        // TODO
+    }
+
+    extractMin() {
+        // TODO
+    }
+
+    bubbleUp(index) {
+        // TODO
+    }
+
+    bubbleDown(index) {
+        // TODO
+    }
+
+    swap(i, j) {
+        // TODO
+    }
+
+    buildHeap(array) {
+        // TODO
+    }
+
+}
